cfd0a2d4933725c52606df4f63b943266ad98cac
Sabrina Evans <sabrina.evans997@hotmail.co.uk>
2022-10-22 20:25:28 +0100

    Add chatbot

---
+++ src/MoneyBot.js
@@ -0,0 +1,14 @@
+import Iframe from 'react-iframe';
+
+export default function MoneyBot() {
+  return (
+    <div>
+      <Iframe
+        url='https://landbot.online/v3/H-1399300-PS53KTE2XA2WIUKJ/index.html'
+        width='500px'
+        height='500px'
+        display='block'
+      />
+    </div>
+  );
+}
